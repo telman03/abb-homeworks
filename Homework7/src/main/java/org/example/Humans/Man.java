3f52b192f6df0ddac434f1efbcf9e0947b580d0f
telman03 <telmangadimov1@gmail.com>
2022-08-11 15:01:08 +0400

    update

---
+++ Homework7/src/main/java/org/example/Humans/Man.java
@@ -0,0 +1,28 @@
+package org.example.Humans;
+
+import org.example.Family;
+import org.example.Human;
+
+
+public final class Man extends Human {
+    public Man(){
+
+    }
+
+    public Man(String name, String surname, int year, int iq, String[][] schedule, Family family) {
+        super(name, surname, year, iq, schedule, family);
+    }
+
+    public Man(String name, String surname, int year, int iq) {
+        super(name, surname, year, iq);
+    }
+
+    @Override
+    public void greetPet() {
+        System.out.println("Hello from Mr." + this.getName());
+    }
+
+    public void playingFootball() {
+        System.out.println("I will play football with my friends");
+    }
+}
---
+++ Homework7/src/main/java/org/example/Humans/Woman.java
@@ -0,0 +1,8 @@
+package org.example.Humans;
+
+import org.example.Family;
+
+public class Woman {
+    public void bornChild(Family maxwellFamily, Man fatherJohn) {
+    }
+}
